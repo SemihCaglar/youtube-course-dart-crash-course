c19141f29202569e48dd161db7a668192c693548
Semih Çağlar <semihcaglar28@gmail.com>
2023-12-07 17:18:57 +0300

    add chapter about operators

---
+++ operators/bin/example1.dart
@@ -0,0 +1,8 @@
+void main(List<String> args) {
+  int a = 12, b = 5;
+  double divided = a / b;
+  int intDivided = a ~/ b;
+  print(divided); // 2.4
+  print(intDivided); // 2
+  // 12.5 ~/5.3 = 2
+}
---
+++ operators/bin/example2.dart
@@ -0,0 +1,19 @@
+void main(List<String> args) {
+  /// unary prefix
+  /// unary postfix
+  /// binary infix
+  /// compund operator
+
+  int age = 5;
+  --age;
+  ++age;
+  ~age;
+  -age;
+  bool b = true;
+  !false;
+
+ /***************/
+  age++;
+  age--;
+  
+}
